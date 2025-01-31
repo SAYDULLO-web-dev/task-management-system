3702ef5566cf806c252c032221485f21a8df8dbd
SaydulloCodeCamper <saydullojonikulov@gmail.com>
2025-01-31 10:20:33 +0900

    test commit

---
+++ src/test/java/web_cybertron/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package web_cybertron.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
